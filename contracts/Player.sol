3763aba317e23bd49f59265f04f65ff954e78d42
Realgue <realague@gmail.com>
2021-10-21 14:00:16 +0200

    WIP

---
+++ contracts/Player.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol';
+
+contract PlayerFactory {
+    
+    struct Player {
+        uint rarity;
+        uint xp;
+        uint id;
+        uint imageId;
+    }
+    
+    createPlayer() {
+        
+    }
+    
+}
---
+++ contracts/WON.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
+
+contract Won is ERC20 {
+
+    constructor() ERC20 ("WON", "WON") {
+        _mint(msg.sender, 200000);
+    }
+    
+}
+
